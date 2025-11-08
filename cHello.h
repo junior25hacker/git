8f188470064348231fe9677f524c7108951c3a3a
root <root@localhost.localdomain>
2025-11-08 15:10:40 -0500

    adding hello.h and hello.c++ file

---
+++ cHello.h
@@ -0,0 +1,14 @@
+#define CHELLO_H
+#ifdef CHELLO_H
+#include<iostream>
+void hellow()
+{
+    int i;
+    for(i=0;i<10;i++)
+    {
+        std::cout << "Hello, World!" << std::endl;
+
+    }
+
+}
+#endif CHELLO_H
---
+++ hello.c++
@@ -0,0 +1,10 @@
+
+#include<iostream>
+#include "cHello.h" 
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    hellow();
+
+    return 0;
+}
