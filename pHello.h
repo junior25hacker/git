aca14dce1cbaedf9ad62e370c7cad7ca850e7f88
perez <root@localhost.localdomain>
2025-11-08 15:20:53 -0500

    added pHello.h

---
+++ pHello.h
@@ -0,0 +1,14 @@
+#define PHELLO_H
+#ifdef PHELLO_H
+#include<iostream>
+void hellOo()
+{
+    int i;
+    for(i=0;i<5;i++)
+    {
+        // Print Hello, Universe! five times
+        std::cout << "Hello, Universe!" << std::endl;
+
+    }
+}
+#endif PHELLO_H
